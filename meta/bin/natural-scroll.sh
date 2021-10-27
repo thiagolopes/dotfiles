c18b86e783685bbc8406591ae918f80b50d6cf22
thiagolopes <thiagolopes@protonmail.com>
2021-10-27 20:26:20 -0300

    add natural scroll config

---
+++ meta/bin/natural-scroll.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+
+id=`xinput list | grep -i "MX Vertical" | cut -d'=' -f2 | cut -d'[' -f1 | cut -d $'\n' -f1`
+natural_scrolling_id=`xinput list-props ${id} | \
+                      grep -i "Natural Scrolling Enabled (" \
+                      | cut -d'(' -f2 | cut -d')' -f1`
+xinput set-prop $id ${natural_scrolling_id} 1
