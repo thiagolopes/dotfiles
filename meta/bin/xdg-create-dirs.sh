f954e804c8902d15f2e67b46903ddb51490eeb92
thiagolopes <thiagolopes@protonmail.com>
2024-02-21 01:45:14 -0300

    Create xdg-create-dirs

---
+++ meta/bin/xdg-create-dirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ~/.config/user-dirs.dirs
+
+mkdir -p $XDG_DESKTOP_DIR
+mkdir -p $XDG_DOWNLOAD_DIR
+mkdir -p $XDG_TEMPLATES_DIR
+mkdir -p $XDG_PUBLICSHARE_DIR
+mkdir -p $XDG_DOCUMENTS_DIR
+mkdir -p $XDG_MUSIC_DIR
+mkdir -p $XDG_PICTURES_DIR
+mkdir -p $XDG_VIDEOS_DIR
+mkdir -p $XDG_DEV_DIR
