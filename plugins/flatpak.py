2a68d68e0972fc0fa2caa2b0104d66f197415f86
thiagolopes <thiagolopes@protonmail.com>
2025-05-28 12:22:30 -0300

    plugin: refator flatpak to deal package by package

--- plugins/flatpak.py
+++ plugins/flatpak.py
@@ -5,6 +5,8 @@ import subprocess
 
 class Flatpak(Plugin):
     _directive = "flatpak"
+    _command = "flatpak install -y --user {package}"
+    _command_repo = "flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo"
 
     def can_handle(self, directive):
         return self._directive == directive
@@ -13,6 +15,20 @@ class Flatpak(Plugin):
         if self._directive != directive:
             raise ValueError("flatpak cannot handle")
         return self._process(data)
+
+    def process_install(self, package):
+        command = self._command.format(package=package)
+        return_process = subprocess.run(command.split(" "), capture_output=True)
+
+        if return_process.returncode == 1:
+            err = return_process.stderr.decode().split("\n")
+            err = "Flatpak:" + "\n".join([e for e in err if e.startswith("error:")])
+            self._log.error(err)
+            return False
+
+        ret = return_process.stderr.decode().rstrip().strip().replace("\n", " ")
+        self._log.lowinfo(ret)
+        return True
 
     def _process(self, data):
         if not which("flatpak"):
@@ -30,22 +46,12 @@ class Flatpak(Plugin):
         if isinstance(packages, str):
             packages = packages.split(" ")
 
-        command = "flatpak install {} -y".format(" ".join(packages))
-        self._log.debug(command)
+        for package in packages:
+            self.process_install(package)
 
-        return_process = subprocess.run(command.split(" "), capture_output=True)
-        if return_process.returncode == 1:
-            err = return_process.stderr.decode().split("\n")
-            err = "Flatpak:\n" + "\n".join([e for e in err if e.startswith("error:")])
-            self._log.error(err)
-            return False
-
-        ret = return_process.stderr.decode().rstrip().strip()
-        self._log.lowinfo(ret)
         self._log.info("Flatpak finished")
         return True
 
     def configure_flathub(self):
-        command = "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo"
-        ret = subprocess.run(command.split(" "), capture_output=True)
-        self._log.info("Flathub configured")
+        ret = subprocess.run(self._command_repo.split(" "), capture_output=True)
+        self._log.info("Flathub repo configured")
